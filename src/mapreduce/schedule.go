9beb9445c365fe633d10513eb1267a34602d818b
maxxx950 <maxxx950@umn.edu>
2018-03-09 17:23:01 -0600

    lab 1 mapreduce

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -2,6 +2,7 @@ package mapreduce
 
 import (
 	"fmt"
+	"sync"
 )
 
 //
@@ -18,62 +19,13 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	var n_other int // number of inputs (for reduce) or outputs (for map)
 
 	switch phase {
-		case mapPhase:
-			ntasks = len(mapFiles)
-			n_other = nReduce
-			// map phase, assign tasks to worker,
-
-			// find map task
-			// keep reading workers address
-			c := 0 // file counter
-
-			for ;c < ntasks; { // ntasks, each task should be assigned to a worker
-				workerRPCAddress, ok := <- registerChan
-				if !ok {
-					continue
-				} else {
-					fmt.Println("assigning map work to worker in: ", workerRPCAddress)
-				}
-
-				c++
-
-				args := DoTaskArgs{jobName,
-				mapFiles[c],
-				phase,
-				c,
-				n_other}
-				// use workerRPCAddress to initiate a call to assign work to the worker
-				go call(workerRPCAddress, "Worker.DoTask", args ,nil)
-			}
-
-		case reducePhase:
-			ntasks = nReduce
-			n_other = len(mapFiles)
-
-			i := 0
-
-			for ; i < ntasks; {
-				workerRPCAddress, ok := <- registerChan
-				if !ok {
-					continue
-					} else {
-					fmt.Println("assigning reduce work to worker in: ", workerRPCAddress)
-				}
-
-				i++
-
-				args := DoTaskArgs{jobName,
-				"", // the fileName arg is not used for reduce task for worker
-				phase,
-				i,
-				n_other}
-
-				go call(workerRPCAddress, "Worker.DoTask", args, nil)
-			}
+	case mapPhase:
+		ntasks = len(mapFiles)
+		n_other = nReduce
+	case reducePhase:
+		ntasks = nReduce
+		n_other = len(mapFiles)
 	}
-
-
-
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
@@ -82,5 +34,47 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	// find map task
+	// keep reading workers address
+	var wg sync.WaitGroup;// used to wait all the goroutine to be finished.
+
+	for c := 0; c < ntasks; c++ { // ntasks, each task should be assigned to a worker
+		var f string
+
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			workerRPCAddress, ok := <-registerChan
+			if !ok {
+				fmt.Printf("%s\n", "error found when try to read from channel")
+			} else {
+				fmt.Println("assigning map work to worker in: ", workerRPCAddress)
+			}
+
+			if phase == mapPhase {
+				f = mapFiles[i]
+			}
+
+			args := DoTaskArgs{
+				jobName,
+				f,
+				phase,
+				i,
+				n_other,
+			}
+			// use workerRPCAddress to initiate a call to assign work to the worker
+			success := call(workerRPCAddress, "Worker.DoTask", args, nil)
+			for !success {
+				newWorker := <- registerChan
+				success = call(newWorker, "Worker.DoTask", args, nil)
+			}
+		 	go func() {
+		 		registerChan <- workerRPCAddress
+		 		}()
+		}(c)
+	}
+	wg.Wait() // wait all the goroutine being executed.
+
 	fmt.Printf("Schedule: %v done\n", phase)
 }
